3cabadf2ce27af6c4725a060bf64e0ac7dd5609d
bigml <bigmaliang@gmail.com>
2010-05-31 21:56:25 +0800

    lcs - chat modify

--- pop/fly/js/ape/app/bmoon.lcs.chat.js
+++ pop/fly/js/ape/app/bmoon.lcs.chat.js
@@ -8,7 +8,11 @@ bmoon.lcs.chat = {
 		html = [
 			'<div id="bchat">',
 				'<div id="bchat-head"><a href="javascript: void(0);" id="bchat-trigger">留言</a></div>',
-				'<div id="bchat-body"></div>',
+				'<div id="bchat-body">',
+					'<div id="bchat-msgs"></div>',
+					'<textarea cols="34" rows="3" id="bchat-input"></textarea>',
+					'<div><a title="Control+Enter" href="javascript:" id="bchat-snd">发送</a></div>',
+				'</div>',
 			'</div>'
 		].join('');
 		if (o.inited) return;
@@ -17,8 +21,24 @@ bmoon.lcs.chat = {
         $('body').append(html);
         if ($.browser.msie && $.browser.version=="6.0") $('#bchat').css('position','absolute');
 
+		$('#bchat-trigger').toggle(o.openChat, o.closeChat);
+		/*
+		$('#bchat-trigger').click(
+		function(){
+			$('#bchat-body').toggle();
+		});
+		 */
+
 		o.ape = ape;
 
 		o.inited = 1;
+	},
+
+	openChat: function() {
+		$('#bchat-body').fadeIn();
+	},
+
+	closeChat: function() {
+		$('#bchat-body').fadeOut();
 	}
 };
