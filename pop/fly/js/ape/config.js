3747d2accaa256e70b1559463c2c447efe544968
bigml <bigmaliang@gmail.com>
2010-06-30 11:05:38 +0800

    pop - ape jsf buildable now

---
+++ pop/fly/js/ape/config.js
@@ -0,0 +1,11 @@
+/***
+ * APE JSF Setup
+ */
+APE.Config.baseUrl = 'http://js.kaiwuonline.com/ape'; //APE JSF 
+APE.Config.domain = 'kaiwuonline.com'; //Your domain, must be the same than the domain in aped.conf of your server
+APE.Config.server = 'push.kaiwuonline.com'; //APE server URL
+
+(function(){
+	for (var i = 0; i < arguments.length; i++)
+		APE.Config.scripts.push(APE.Config.baseUrl + '/Build/' + arguments[i] + '.js');
+})('apeCore');
