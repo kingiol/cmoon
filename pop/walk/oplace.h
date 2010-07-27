90c189a327c879f70b2e30aa9ae9092dc70b3db6
bigml <bigmaliang@gmail.com>
2010-07-27 11:55:01 +0800

    pop - issue 2: ip to place UE imporve.

--- pop/walk/oplace.h
+++ pop/walk/oplace.h
@@ -8,6 +8,7 @@ __BEGIN_DECLS
 #define	REDIRECT_MODE_2 0x02
 #define IP_RECORD_LENGTH 7
 
+int ip2addr_data_get(char *ip, char **c, char **a);
 int place_data_get(CGI *cgi, HASH *dbh, HASH *evth, session_t *ses);
 
 __END_DECLS
