c6b245932b42f94e85b37ce89a908292b8db0be7
bigml <bigmaliang@gmail.com>
2010-07-31 17:26:05 +0800

    pop - chat client load by a single js file(bmoon.kol.js)

---
+++ pop/fly/js/bmoon.kol.js
@@ -0,0 +1,61 @@
+; var bmoon = bmoon || {};
+bmoon.kol = {
+	version: "1.0",
+
+	_loadJs: function(url) {
+		var e = document.createElement("script");
+		e.src = url;
+		e.type="text/javascript";
+		document.getElementsByTagName("head")[0].appendChild(e);
+	},
+
+	_execAfter: function(func, until) {
+		if (eval(until)) {
+			func();
+		} else {
+			setTimeout(function() {
+				bmoon.kol._execAfter(func, until);
+			}, 100);
+		}
+	},
+	
+	// {aname: 'xxx'}
+	init: function(opt) {
+		var o = bmoon.kol;
+
+		if (o.inited) return o;
+		o.inited = true;
+
+		o.aname = opt.aname;
+
+		if (typeof jQuery != 'function') {
+			o._loadJs('http://js.kaiwuonline.com/b/chatb.js');
+		} else if (typeof jQuery.cookie != 'function') {
+			$('head').append('<script type="text/javascript" src="http://js.kaiwuonline.com/b/chatc.js"></script>');
+		}
+		
+		// wait for chatx.js loaded
+		o._execAfter(function() {
+			$('head').append('<link rel="stylesheet" href="http://css.kaiwuonline.com/b/chat.css" />');
+			$('head').append('<!--[if IE 6]><link rel="stylesheet" href="http://css.kaiwuonline.com/b/chat_ie6.css" /><![endif]-->');
+			$('head').append('<script type="text/javascript" src="http://js.kaiwuonline.com/b/chat.js"></script>');
+		}, "typeof jQuery == 'function' && typeof jQuery.cookie == 'function'");
+		
+		return o;
+	},
+
+	onready: function(opt) {
+		var o = bmoon.kol.init(opt);
+
+		o._execAfter(function() {
+			var client = new APE.Client();
+			client.load({
+				identifier: o.aname,
+				transport: 2,
+				complete: function(ape) {
+					new liveCS(ape).initialize({aname: o.aname});
+				}
+			});
+		}, "typeof bmoon.chat == 'object'");
+	}
+};
