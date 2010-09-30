271d984890f36463201c243a0fe2ca083dfa8941
bigml <bigmaliang@gmail.com>
2010-09-30 14:31:02 +0800

    pop - yellow url script add

---
+++ pop/mtls/yurl/conv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+OUTDIR=../aaaU/
+
+for file in *.html; do
+	echo "Process ${file}..."
+	iconv -f gb2312 -t utf8 ${file} > ${OUTDIR}${file}
+done
---
+++ pop/mtls/yurl/get_i.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BGN=0
+END=500000
+
+for i in `seq ${BGN} ${END}`; do
+	echo ">>>Process $i ..."
+	wget --quiet --timeout=5 --tries=1 http://www.yellowurl.cn/${i}.html
+	sleep 1
+done
+
+echo "################## DONE ##################"
