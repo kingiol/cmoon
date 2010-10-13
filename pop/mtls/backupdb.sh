105d5104f79b4c05894816a664db95af860dc271
bigml <bigmaliang@gmail.com>
2010-10-13 13:15:22 +0800

    pop - mtls script add

---
+++ pop/mtls/backupdb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CUR_DATE=`date +"%F"`
+BACKUP_DIR=/data/dbs/
+
+pg_dump -U lcser lcs_main > ${BACKUP_DIR}${CUR_DATE}.lcs_main
+pg_dump -U lcser lcs_dyn > ${BACKUP_DIR}${CUR_DATE}.lcs_dyn
+pg_dump -U lcser lcs_msg > ${BACKUP_DIR}${CUR_DATE}.lcs_msg
+pg_dump -U lcser lcs_mtool > ${BACKUP_DIR}${CUR_DATE}.lcs_mtool
