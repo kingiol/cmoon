f3246072331d77b52694ce7fe2adb99ae6043878
bigml <bigmaliang@gmail.com>
2010-10-13 12:54:20 +0800

    pop - okol stat script add

--- pop/mtls/stat.sh
+++ pop/mtls/stat.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-psql -U lcser lcs_main -c "SELECT COUNT(*) FROM appinfo WHERE intime > current_date -1;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 0, * FROM count_app_yesterday;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 1, * FROM count_user_yesterday;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 2, * FROM count_join_yesterday;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 3, * FROM count_visit_yesterday;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 4, * FROM count_msg_yesterday;"
+
+
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 100, * FROM count_app_total;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 101, * FROM count_user_total;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 102, * FROM count_join_total;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 103, * FROM count_visit_total;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 104, * FROM count_msg_total;"
