10711effbef7967b53799c5b7676413e53a1b215
bigml <bigmaliang@gmail.com>
2010-10-15 10:52:45 +0800

    pop - add count_app_vaid_xxx view

--- pop/mtls/stat.sh
+++ pop/mtls/stat.sh
@@ -5,6 +5,7 @@ psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 1, * FROM coun
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 2, * FROM count_join_today;"
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 3, * FROM count_visit_today;"
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 4, * FROM count_msg_today;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 5, * FROM count_app_valid_today;"
 
 
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 100, * FROM count_app_total;"
@@ -12,3 +13,4 @@ psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 101, * FROM co
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 102, * FROM count_join_total;"
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 103, * FROM count_visit_total;"
 psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 104, * FROM count_msg_total;"
+psql -U lcser lcs_mtool -c "INSERT INTO stat (type, count) SELECT 105, * FROM count_app_valid_total;"
--- pop/mtls/view_mtls.sql
+++ pop/mtls/view_mtls.sql
@@ -3,18 +3,21 @@ DROP VIEW count_user_yesterday;
 DROP VIEW count_join_yesterday;
 DROP VIEW count_visit_yesterday;
 DROP VIEW count_msg_yesterday;
+DROP VIEW count_app_valid_yesterday;
 
 DROP VIEW count_app_today;
 DROP VIEW count_user_today;
 DROP VIEW count_join_today;
 DROP VIEW count_visit_today;
 DROP VIEW count_msg_today;
+DROP VIEW count_app_valid_today;
 
 DROP VIEW count_app_total;
 DROP VIEW count_user_total;
 DROP VIEW count_join_total;
 DROP VIEW count_visit_total;
 DROP VIEW count_msg_total;
+DROP VIEW count_app_valid_total;
 
 
 CREATE VIEW count_app_yesterday AS SELECT * FROM dblink('dbname=lcs_main host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM appinfo WHERE intime > current_date -1 AND intime < current_date') AS t1(cc int);
@@ -22,15 +25,18 @@ CREATE VIEW count_user_yesterday AS SELECT * FROM dblink('dbname=lcs_main host=l
 CREATE VIEW count_join_yesterday AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM lcsjoin WHERE intime > current_date -1 AND intime < current_date') AS t1(cc int);
 CREATE VIEW count_visit_yesterday AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM visit WHERE intime > current_date -1 AND intime < current_date') AS t1(cc int);
 CREATE VIEW count_msg_yesterday AS SELECT * FROM dblink('dbname=lcs_msg host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM msg WHERE intime > current_date -1 AND intime < current_date') AS t1(cc int);
+CREATE VIEW count_app_valid_yesterday AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(DISTINCT aname) FROM lcsjoin WHERE intime > current_date -1 AND intime < current_date') AS t1(cc int);
 
 CREATE VIEW count_app_today AS SELECT * FROM dblink('dbname=lcs_main host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM appinfo WHERE intime > current_date') AS t1(cc int);
 CREATE VIEW count_user_today AS SELECT * FROM dblink('dbname=lcs_main host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM userinfo WHERE intime > current_date') AS t1(cc int);
 CREATE VIEW count_join_today AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM lcsjoin WHERE intime > current_date') AS t1(cc int);
 CREATE VIEW count_visit_today AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM visit WHERE intime > current_date') AS t1(cc int);
 CREATE VIEW count_msg_today AS SELECT * FROM dblink('dbname=lcs_msg host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM msg WHERE intime > current_date') AS t1(cc int);
+CREATE VIEW count_app_valid_today AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(DISTINCT aname) FROM lcsjoin WHERE intime > current_date') AS t1(cc int);
 
 CREATE VIEW count_app_total AS SELECT * FROM dblink('dbname=lcs_main host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM appinfo') AS t1(cc int);
 CREATE VIEW count_user_total AS SELECT * FROM dblink('dbname=lcs_main host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM userinfo') AS t1(cc int);
 CREATE VIEW count_join_total AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM lcsjoin') AS t1(cc int);
 CREATE VIEW count_visit_total AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM visit') AS t1(cc int);
 CREATE VIEW count_msg_total AS SELECT * FROM dblink('dbname=lcs_msg host=localhost user=lcser password=loveu', 'SELECT COUNT(*) FROM msg') AS t1(cc int);
+CREATE VIEW count_app_valid_total AS SELECT * FROM dblink('dbname=lcs_dyn host=localhost user=lcser password=loveu', 'SELECT COUNT(DISTINCT aname) FROM lcsjoin') AS t1(cc int);
