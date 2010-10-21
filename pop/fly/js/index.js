0f9ded62badfeb4aba14d8a984ab4e134c97baca
bigml <bigmaliang@gmail.com>
2010-10-21 17:45:01 +0800

    pop - begin to change index

---
+++ pop/fly/js/index.js
@@ -0,0 +1,27 @@
+; var bmoon = bmoon || {};
+bmoon.index = {
+	version: '1.0',
+	
+	init: function() {
+		var o = bmoon.index;
+		if (o.inited) return o;
+
+		o.scroll = $(".scrollable").scrollable({api: true});
+		
+		o.inited = true;
+		return o;
+	},
+	
+	onready: function() {
+		var o = bmoon.index.init();
+
+		o.bindClick();
+	},
+
+	bindClick: function() {
+		var o = bmoon.index.init();
+
+	}
+};
+
+$(document).ready(bmoon.index.onready);
