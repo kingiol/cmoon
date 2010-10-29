cab9ce280c6293c9c92d4b117cc2aaa358c70ce4
bigml <bigmaliang@gmail.com>
2010-10-29 23:12:14 +0800

    pop - email daemon bug fix

--- pop/mtls/email.sh
+++ pop/mtls/email.sh
@@ -20,7 +20,7 @@ FS="|"
 
 
 # 根据参数 select 对应的数据到 临时文件
-psql -U lcser lcs_aux -t -F${FS} -c "SELECT max(opts), max(subject), str_concat(sendto || ', '), str_concat(cc || ', '), str_concat(bcc || ', '), checksum, str_concat(id || ',') FROM email WHERE state=0 AND gotime="${GOTIME}" GROUP BY checksum" > ${TMPFILE}
+psql -U lcser lcs_aux -t -F${FS} -c "SELECT max(opts), max(subject), str_concat(sendto || ','), str_concat(cc || ','), str_concat(bcc || ','), checksum, str_concat(id || ',') FROM email WHERE state=0 AND gotime="${GOTIME}" GROUP BY checksum" > ${TMPFILE}
 
 # readline 临时文件， 分行处理
 # awk -F: 'script' / -f scriptfile FILE  对FILE逐行进行script操作
@@ -32,23 +32,29 @@ cat ${TMPFILE} | while read oneline
 do
 	legal=`echo ${oneline} | awk -F${FS} '{if (NF==7) {print "ok"} else {print "illegal"}}'`
 	if [ "${legal}" = "ok" ]; then
-		checksum=`echo ${oneline} | awk -F${FS} '{ print $6}'`
-		psql -U lcser lcs_aux -t -c "SELECT content FROM email WHERE checksum='${checksum}' LIMIT 1" > ${TMPFILEMAIL}
+		checksum=`echo ${oneline} | awk -F${FS} '{print $6}'`
+		checksum=${checksum## }
+		checksum=${checksum%% }
+		psql -U lcser lcs_aux -t -c "SELECT content FROM email WHERE checksum='"${checksum}"' LIMIT 1" > ${TMPFILEMAIL}.${checksum}
 		
-		ids=`echo ${oneline} | awk -F${FS} '{ print $7}'`
+		ids=`echo ${oneline} | awk -F${FS} '{print $7}'`
 		idsl=$((${#ids}-1))
 #		ids=$(expr substr ${ids} 0 ${idsl})
 		ids=${ids:0:${idsl}}
-		cmd=`echo ${oneline} | awk -F${FS} '{ print "/usr/local/bin/email " $1 " -s " $2 " -cc " $4 " -bcc " $5 $3}'`
-		echo -n "send mail for ${ids}... "
-		${cmd} < ${TMPFILEMAIL}
+# 当前不支持 cc, bcc。 因为-cc , , , -bcc , , ,会让email报 Smtp error: 503 5.5.1 RCPT first 错误
+# 而， sub(/,+/, ",", $4) 又不起作用。
+#		cmd=`echo ${oneline} | awk -F${FS} '{print "/usr/local/bin/email " $1 " -s " $2 " -cc " $4 " -bcc " $5 $3}'`
+		cmd=`echo ${oneline} | awk -F${FS} '{print "/usr/local/bin/email " $1 " -s " $2 $3}'`
+		echo "send mail for ${ids}..."
+#		echo ${cmd}
+		${cmd} < ${TMPFILEMAIL}.${checksum} 2>&1 | grep 'FATAL'
 		if [ $? = 0 ]; then
+			echo "failure ${ret}."
+		else
 			echo "done"
 			psql -U lcser lcs_aux -c "UPDATE email SET state=1 WHERE id IN (${ids})"
-		else
-			echo "failure $?."
 		fi
-	else
+	elif [ ${#ids} -gt 1 ]; then
 		echo "${oneline} illegal"
 	fi
 done
