c6b8d66bac2e67589908e69c95b5995641004df5
bigml <bigmaliang@gmail.com>
2010-11-15 17:08:42 +0800

    pop - doc of chat client develop

--- pop/fly/js/bmoon.kol.js
+++ pop/fly/js/bmoon.kol.js
@@ -38,11 +38,11 @@ bmoon.kol = {
 		_execAfter(function() {
 			opts = $.extend({
 				aname: 'unknown',
-				css: 'http://css.kaiwuonline.com/b/client/lcs'
+				css: 'http://css.kaiwuonline.com/b/client/lcs.css'
 			}, opts || {});
 			
 			$('head').append('<link rel="stylesheet" href="' + opts.css + '.css" />');
-			$('head').append('<!--[if IE 6]><link rel="stylesheet" href="' + opts.css + '_ie6.css" /><![endif]-->');
+			$('head').append('<!--[if IE 6]><link rel="stylesheet" href="' + opts.css.substr(0, opts.css.length-4) + '_ie6.css" /><![endif]-->');
 			var unode = $('meta[content*="utf"]');
 			if (!unode.length) unode = $('meta[content*="UTF"]');
 			if (unode.length) {
