89756b066aacc12bdffba6181eed9ffa1a91c44c
bigml <bigmaliang@gmail.com>
2010-12-25 22:04:02 +0800

    pop - oms/stat add stat-type select

--- pop/fly/js/oms/stat.js
+++ pop/fly/js/oms/stat.js
@@ -19,6 +19,7 @@ bmoon.omsstat = {
 		var o = bmoon.omsstat;
 		if (o.inited) return o;
 
+		o.sttype = $('select[name=stat-type]');
 		o.stvisit = $('#st-visit');
 		o.stbar = $('#st-bar');
 		o.stdata = mgd.stdata;
@@ -31,36 +32,11 @@ bmoon.omsstat = {
 		var o = bmoon.omsstat.init();
 
 		o.bindClick();
-		
+
 		// avoid js error, which will lead login overlay don't load
 		if (!o.stdata) return;
+		o.rendStat();
 
-		$.plot(o.stvisit,
-			   [
-				   {label: '日访问量', data: o.stdata.pv},
-				   {label: '日用户量', data: o.stdata.uv}
-			   ],
-			   {
-				   xaxis: {mode: 'time', timeformat: "%y-%m-%d"},
-				   series: {
-					   lines: {show: true},
-					   points: {show:true}
-				   },
-				   grid: {hoverable: true}
-			   });
-
-		$.plot(o.stbar,
-			   [
-				   {label: '来源url', data: o.stdata.refer},
-				   {label: '页面访问次数', data: o.stdata.url},
-				   {label: '地区访问者', data: o.stdata.area}
-			   ],
-			   {
-				   series: {
-					   bars: {show: true, barWidth: 0.6}
-				   },
-				   grid: {hoverable: true}
-			   });
 	},
 
 	bindClick: function() {
@@ -69,6 +45,37 @@ bmoon.omsstat = {
 		$('.datep').datePicker({startDate: '2010-10-10'});
 		o.stvisit.bind('plothover', o.visitOver);
 		o.stbar.bind('plothover', o.barOver);
+		o.sttype.change(o.rendStat);
+	},
+
+	rendStat: function() {
+		var o = bmoon.omsstat.init();
+
+		var type = o.sttype.val();
+		
+		if (type == 'visit') {
+			$.plot(o.stvisit,
+				   [
+					   {label: '日访问量', data: o.stdata.pv},
+					   {label: '日用户量', data: o.stdata.uv}
+				   ],
+				   {
+					   xaxis: {mode: 'time', timeformat: "%y-%m-%d"},
+					   series: {
+						   lines: {show: true},
+						   points: {show:true}
+					   },
+					   grid: {hoverable: true}
+				   });
+		} else {
+			$.plot(o.stbar, o.stdata[type],
+				   {
+					   series: {
+						   bars: {show: true, barWidth: 0.6}
+					   },
+					   grid: {hoverable: true}
+				   });
+		}
 	},
 
 	visitOver: function(event, pos, item) {
@@ -108,7 +115,7 @@ bmoon.omsstat = {
                 y = item.datapoint[1];
                 
                 o._showTooltip(item.pageX, item.pageY,
-                               o.stdata.aux[x] + ' ' + item.series.label + ' : ' + y);
+                               o.stdata.aux[x] + ' ' + ' : ' + y);
             }
         } else {
             $("#tooltip").remove();
