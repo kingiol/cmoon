a07c4900cf11d1f9ac48042049e8a0e698b81cae
moon <moon@debian.(none)>
2010-12-27 17:01:47 +0800

    pop - chat admin js bug fix

--- pop/fly/js/ape/app/bmoon.chatb.js
+++ pop/fly/js/ape/app/bmoon.chatb.js
@@ -206,7 +206,8 @@ bmoon.chat = {
 		}
 
 		if ($.inArray(id, o.usersFetched) == -1) {
-			o.getMessage(id, o.aname, 1);
+			// wait some time for message insert done
+			setTimeout(function() {o.getMessage(id, o.aname, 1);}, 500);
 			o.usersFetched.push(id);
 		}
 
