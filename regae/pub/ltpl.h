c0f5d35433188bc976c6a4e91a0e30bce1f8da61
bigml <bigmaliang@gmail.com>
2011-07-12 17:46:50 +0800

    regae - init version

---
+++ regae/pub/ltpl.h
@@ -0,0 +1,16 @@
+#ifndef __LTPL_H__
+#define __LTPL_H__
+#include "mheads.h"
+
+__BEGIN_DECLS
+
+void ltpl_prepare_rend(HDF *hdf, char *tpl);
+
+void ltpl_destroy(HASH *tplh);
+NEOERR* ltpl_init(HASH **tplh);
+NEOERR* ltpl_render(CGI *cgi, HASH *tplh, session_t *ses);
+NEOERR* ltpl_parse_file(HASH *dbh, void *lib, char *dir, char *name, HASH *outhash);
+NEOERR* ltpl_parse_dir(char *dir, HASH *outhash);
+
+__END_DECLS
+#endif	/* __LTPL_H__ */
