8de6a216409ca92f60ac797a81a08dcd69521dbd
bigml <bigmaliang@gmail.com>
2012-02-13 15:21:40 +0800

    regae - pager fix

--- regae/pager/static.c
+++ regae/pager/static.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     err = mcfg_parse_file(SITE_CONFIG, &g_cfg);
     DIE_NOK_MTL(err);
 
-    err = ltpl_parse_file(NULL, NULL, PATH_TPL, "config/static/static.hdf", NULL);
+    err = ltpl_parse_dir(PATH_TPL"config/static", NULL);
     DIE_NOK_MTL(err);
 
     return 0;
