708e8458b7b526be6f368095fa1db1ecb60d9607
xiangyuan <liyajie1209@gmail.com>
2012-05-10 11:25:14 +0800

    添加json处理框架，整个工程不再支持自动引用.

---
+++ phone/ios/cmoon/cmoon/DidaViewController.h
@@ -0,0 +1,13 @@
+//
+//  DidaViewController.h
+//  cmoon
+//
+//  Created by li yajie on 12-5-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DidaViewController : UIViewController
+
+@end
